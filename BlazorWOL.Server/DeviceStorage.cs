d22c14ab9fa6d7de96aa73b6021d7087a483509a
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-01 22:30:54 +0900

    [update]デバイスの永続化処理を実装

--- BlazorWOL.Server/DeviceStorage.cs
+++ BlazorWOL.Server/DeviceStorage.cs
@@ -1,9 +1,67 @@
-﻿using System;
+﻿using BlazorWOL.Shared;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace BlazorWOL.Server {
   public class DeviceStorage {
+    string StoragePath { get; }
+    List<Device> Devices { get; } = new List<Device>();
+
+    public DeviceStorage(string storagePath) {
+      StoragePath = storagePath;
+      if (File.Exists(StoragePath)) {
+        var json = File.ReadAllText(StoragePath);
+        var devices = JsonConvert.DeserializeObject<Device[]>(json);
+        Devices.AddRange(devices);
+      }
+    }
+
+    public List<Device> GetDevices() {
+      lock (this) {
+        return Devices;
+      }
+    }
+
+    public void AddDevice(Device device) {
+      lock (this) {
+        this.Devices.Add(device);
+        FlushToStorage();
+      }
+    }
+
+    private Device GetDevice(Guid id) {
+      lock (this) {
+        return Devices.Find(device => device.Id == id);
+      }
+    }
+
+    public void UpdateDevice(Guid id, Device device) {
+      lock (this) {
+        Device updateTo = GetDevice(id);
+        updateTo.Name = device.Name;
+        updateTo.MACAddress = device.MACAddress;
+        FlushToStorage();
+      }
+    }
+
+    public Device DeleteDevice(Guid id) {
+      lock (this) {
+        Device deleteTo = GetDevice(id);
+        if (deleteTo != null) {
+          Devices.Remove(deleteTo);
+        }
+        FlushToStorage();
+        return deleteTo;
+      }
+    }
+
+    private void FlushToStorage() {
+      lock (this) {
+        var json = JsonConvert.SerializeObject(Devices);
+        File.WriteAllText(StoragePath, json);
+      }
+    }
   }
 }

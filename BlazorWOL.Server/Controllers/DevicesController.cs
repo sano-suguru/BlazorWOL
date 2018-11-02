18a187b03afe4e568ea457144f346fab665e5eb5
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-02 23:02:06 +0900

    [add]DevicesControllerをスキャフォールディング

---
+++ BlazorWOL.Server/Controllers/DevicesController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWOL.Server.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DevicesController : ControllerBase {
+  }
+}

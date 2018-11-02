3a1c6e92076c35290852982a30338919916e45e4
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-02 23:24:41 +0900

    [update]Device取得WebAPIを実装

--- BlazorWOL.Server/Controllers/DevicesController.cs
+++ BlazorWOL.Server/Controllers/DevicesController.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorWOL.Shared;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace BlazorWOL.Server.Controllers {
@@ -15,5 +16,12 @@ namespace BlazorWOL.Server.Controllers {
     [HttpGet]
     public IEnumerable<Device> GetDevices() =>
       Storage.GetDevices();
+
+    [HttpGet, Route("{guid}")]
+    public IActionResult GetDevice(Guid id) {
+      var device = Storage.GetDevice(id);
+      if (device == null) { return NotFound(); }
+      return Ok(device);
+    }
   }
 }

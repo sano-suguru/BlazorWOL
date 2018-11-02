5f5f2ad450eeea6b7b2db0d1b39abc47b1f7680f
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-02 23:08:17 +0900

    [update]Device一覧取得APIを実装

--- BlazorWOL.Server/Controllers/DevicesController.cs
+++ BlazorWOL.Server/Controllers/DevicesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlazorWOL.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BlazorWOL.Server.Controllers {
   [Route("api/[controller]")]
@@ -9,5 +11,9 @@ namespace BlazorWOL.Server.Controllers {
     public DevicesController(DeviceStorage deviceStorage) {
       Storage = deviceStorage;
     }
+
+    [HttpGet]
+    public IEnumerable<Device> GetDevices() =>
+      Storage.GetDevices();
   }
 }
